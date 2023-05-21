60f81439b27eaccd42de831332ffe17b93242904
Kaan Gürbüz <abdikaangrbz@gmail.com>
2023-05-22 01:38:43 +0300

    change

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -5,7 +5,7 @@ const Products = () => {
     <>
       <title>Figma Tasarımı</title>
       <meta name="description" content="Test your website with the free SEO check from Seobility and get tips for a better search engine optimization."></meta>
-      <meta http-equiv="content-language" content="tr"></meta>
+      <meta http-equiv="content-language" content="tr">     </meta>  
     </>
   )
 }
